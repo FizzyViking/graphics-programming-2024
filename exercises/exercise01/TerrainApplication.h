5f207473757744861d67249558790a0a920772aa
alexander.rode2 <alexander.rode2@gmail.com>
2024-02-18 17:10:13 +0100

    Week 2 work

--- exercises/exercise01/TerrainApplication.h
+++ exercises/exercise01/TerrainApplication.h
@@ -3,6 +3,9 @@
 #include <ituGL/application/Application.h>
 
 // (todo) 01.1: Include the libraries you need
+#include <ituGL/geometry/VertexBufferObject.h>
+#include <ituGL/geometry/VertexArrayObject.h>
+#include <ituGL/geometry/ElementBufferObject.h>
 
 
 class TerrainApplication : public Application
@@ -17,11 +20,19 @@ protected:
     void Cleanup() override;
 
 private:
+    void BuildShaders();
+    void UpdateOutputMode();
+
+private:
     unsigned int m_gridX, m_gridY;
 
+    unsigned int m_shaderProgram;
+
     // (todo) 01.1: Declare an VBO, VAO
-
-
+    VertexBufferObject vbo;
+    VertexArrayObject vao;
+    
     // (todo) 01.5: Declare an EBO
+    ElementBufferObject ebo;
 
 };
